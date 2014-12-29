3d736a7adea51d470e669f75197466ee0d708d91
nezharen <nezharen@163.com>
2014-12-29 17:17:39 +0800

    ImageView!

--- gui.c
+++ gui.c
@@ -17,11 +17,11 @@ void initGUI()
   GUI_INFO = *((GUI_MODE_INFO *)(GUI_BUF << 4));
 }
 
-void drawWindow()
+void drawWindows()
 {
   RGB *screen_temp = (RGB *)(GUI_INFO.PhysBasePtr + 0x3c0000);
 
-  int i, j;
+  int i, j, k;
   WindowQueue *p = &windowQueue;
 
   while (p->next != 0)
@@ -30,21 +30,28 @@ void drawWindow()
     switchuvm(p->proc);
     if (p->window != 0)
       if (p->window->show == 1)
+      {
         for (i = 0; i < p->window->width; i++)
         {
           for (j = 0; j < CAPTION_HEIGHT; j++)
           {
-            screen_temp[(p->window->leftTopY + j) * SCREEN_WIDTH + p->window->leftTopX + i].R = 0xFF;
-            screen_temp[(p->window->leftTopY + j) * SCREEN_WIDTH + p->window->leftTopX + i].G = 0xFF;
+            screen_temp[(p->window->leftTopY + j) * SCREEN_WIDTH + p->window->leftTopX + i].R = 0x00;
+            screen_temp[(p->window->leftTopY + j) * SCREEN_WIDTH + p->window->leftTopX + i].G = 0x00;
             screen_temp[(p->window->leftTopY + j) * SCREEN_WIDTH + p->window->leftTopX + i].B = 0x00;
           }
           for (j = CAPTION_HEIGHT; j < p->window->height; j++)
           {
-            screen_temp[(p->window->leftTopY + j) * SCREEN_WIDTH + p->window->leftTopX + i].R = 0x00;
-            screen_temp[(p->window->leftTopY + j) * SCREEN_WIDTH + p->window->leftTopX + i].G = 0xFF;
-            screen_temp[(p->window->leftTopY + j) * SCREEN_WIDTH + p->window->leftTopX + i].B = 0x00;
+            screen_temp[(p->window->leftTopY + j) * SCREEN_WIDTH + p->window->leftTopX + i].R = 0xbb;
+            screen_temp[(p->window->leftTopY + j) * SCREEN_WIDTH + p->window->leftTopX + i].G = 0xbb;
+            screen_temp[(p->window->leftTopY + j) * SCREEN_WIDTH + p->window->leftTopX + i].B = 0xbb;
           }
         }
+        for (k = 0; k < p->window->widgetsNum; k++)
+          if (p->window->widgets[k].type == imageView)
+            for (i = 0; i < p->window->widgets[k].context.imageView->width; i++)
+              for (j = 0; j < p->window->widgets[k].context.imageView->height; j++)
+                screen_temp[(p->window->widgets[k].context.imageView->leftTopY + j) * SCREEN_WIDTH + p->window->widgets[k].context.imageView->leftTopX + i] = p->window->widgets[k].context.imageView->image[j * p->window->widgets[k].context.imageView->height + i];
+      }
     switchkvm();
   }
 }
@@ -56,8 +63,8 @@ void updateGUI()
   int totalPels = SCREEN_WIDTH * SCREEN_HEIGHT;
   int i;
 
-  memset(screen_temp, 0, sizeof(RGB) * totalPels);
-  drawWindow();
+  memset(screen_temp, 0xff, sizeof(RGB) * totalPels);
+  drawWindows();
 
   for (i = 0; i < totalPels; i++)
     screen[i] = screen_temp[i];
